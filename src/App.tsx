b9c76081d7e93cbbea5dbc87a3f4427c3dfdd7dd
morpheus <jjkk186@googlemail.com>
2021-11-04 13:48:56 +0100

    use Layout component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { Header } from "./components/Layout/Header/Header";
+import { Layout } from "./components/Layout/Layout";
 import { Selector } from "./components/Selector/Selector";
 import ValueList from "./values.json";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header height={16} />
-
-      <main className="h-screen pt-16 flex flex-grow">
-        <Selector nbOfElements={10} list={ValueList} absoluteHeight={false} />
-      </main>
-    </div>
+    <Layout>
+      <Selector nbOfElements={10} list={ValueList} absoluteHeight={false} />
+    </Layout>
   );
 }
 
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { FC } from "react";
-import { Header } from "./Header/Header";
+import { Header as FixedHeader } from "./Header/Header";
 
 export const Layout: FC<{}> = ({ children }) => {
   return (
-    <div className="">
-      <Header height={16}/>
-      <main className="">{children}</main>
+    <div className="flex flex-col min-h-screen">
+      <FixedHeader height={16} />
+      <main className="h-screen pt-16 flex flex-grow">{children}</main>
     </div>
   );
 };
