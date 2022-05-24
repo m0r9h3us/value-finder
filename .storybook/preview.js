e7a2e060eec6be54ae7ba9606c8d26f01e905701
morpheus <jjkk186@googlemail.com>
2022-05-24 16:03:10 +0200

    update storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,19 @@
 import './index.css';
-import "../src/context/ValueContextProvider"
+import '../src/context/ValueContextProvider';
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+
+addDecorator((story) => (
+  <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+));
+
 export const parameters = {
   layout: 'fullscreen',
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
