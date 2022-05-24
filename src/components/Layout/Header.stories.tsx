05c7a9d5c31f628d1f702faa6196ae6409849559
morpheus <jjkk186@googlemail.com>
2022-05-24 18:45:37 +0200

    update stories

--- src/components/Layout/Header.stories.tsx
+++ src/components/Layout/Header.stories.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Header } from "./Header";
+import { Header } from './Header';
 
 export default {
-  title: "Layout/Header",
+  title: 'Layout/Header',
   component: Header,
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {};
+export const Default = Template.bind({});
+
+Default.args = {
+  title: 'TestTitle',
+};
